c05fad23ca66ddb936067006f7edffb93b202f79
Muhammad Tifak <moht20009@gmail.com>
2020-06-12 23:23:25 -0700

    Rendered GO template & made HTML page

--- makesite.go
+++ makesite.go
@@ -1,17 +1,51 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
+
+	"html/template"
 	"io/ioutil"
+	"os"
 )
 
+type fluff struct {
+	Content string
+}
+
 func main() {
-	fileContents, err := ioutil.ReadFile("first-post.txt")
+	// Files are provided as a slice of strings.
+	paths := []string{
+		"template.tmpl",
+	}
+
+	post := readFile()
+
+	t := template.Must(template.New("template.tmpl").ParseFiles(paths...))
+	file, err := os.Create("first-post.html")
 	if err != nil {
-		// A common use of `panic` is to abort if a function returns an error
-		// value that we don’t know how to (or want to) handle. This example
-		// panics if we get an unexpected error when creating a new file.
 		panic(err)
 	}
-	fmt.Print(string(fileContents))
+
+	err = t.Execute(file, post)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func readFile() string {
+	fileContents, err := ioutil.ReadFile("first-post.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	return string(fileContents)
+}
+
+func writeFile(text string) {
+	bytesToWrite := []byte(text)
+	err := ioutil.WriteFile("first-post.html", bytesToWrite, 0644)
+	if err != nil {
+		panic(err)
+	}
+	return
 }
